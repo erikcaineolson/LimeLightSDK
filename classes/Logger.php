1b58c0a98cb652c2c9299d3f505907be8d1e3d05
Erik C. Olson <erikcaineolson@gmail.com>
2014-01-26 18:32:44 -0800

    Typo Fix

--- classes/Logger.php
+++ classes/Logger.php
@@ -4,20 +4,20 @@
 		private $fh;
 		
 		function __construct($filename, $delimiter){
-			$this->$fh = fopen($filename, 'a');
-			$this->$delim = $delimiter;
+			$this->fh = fopen($filename, 'a');
+			$this->delim = $delimiter;
 		}	// END __construct()
 		
 		function __destruct(){
-			fclose($this->$fh);
+			fclose($this->fh);
 		}	// END __destruct()
 		
 		public function LogError($title, $message){
 			$output = NULL;
 			
-			$output = date('Y-m-d H:i:s') . $this->$delim . 'ERROR' . $this->$delim . $title . $this->$delim . $message . "\n";
+			$output = date('Y-m-d H:i:s') . $this->delim . 'ERROR' . $this->delim . $title . $this->delim . $message . "\n";
 			
-			fwrite($this->$fh, $output);
+			fwrite($this->fh, $output);
 		}	// END LogError
 		
 		public function LogLine($line_array){
@@ -26,20 +26,20 @@
 			$output = date('Y-m-d H:i:s');
 			
 			foreach($line_array as $v){
-				$output .= $this->$delim . $v;
+				$output .= $this->delim . $v;
 			}
 			
 			$output .= "\n";
 			
-			fwrite($this->$fh, $output);
+			fwrite($this->fh, $output);
 		}
 
 		public function LogResponse($title, $message){
 			$output = NULL;
 			
-			$output = date('Y-m-d H:i:s') . $this->$delim . 'RESPONSE' . $this->$delim . $title . $this->$delim . $message . "\n";
+			$output = date('Y-m-d H:i:s') . $this->delim . 'RESPONSE' . $this->delim . $title . $this->delim . $message . "\n";
 			
-			fwrite($this->$fh, $output);
+			fwrite($this->fh, $output);
 		}	// END LogResponse
 	}
 ?>
